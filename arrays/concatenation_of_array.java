ffc55aefd20ddfcd1f475a0891bdaf5e82e1f7ec
Navyachhoker <143236749+Navyachhoker@users.noreply.github.com>
2026-06-15 17:30:51 +0530

    concatenation problem

---
+++ arrays/concatenation_of_array.java
@@ -0,0 +1,24 @@
+package arrays;
+import java.util.Arrays;
+public class concatenation_of_array {
+    
+    public static int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2*n];
+
+        for(int i =0; i<n; i++){
+            ans[i]= nums[i];
+            ans[i+n]= nums[i];
+        }
+
+        return ans;
+    }
+
+    public static void main(String args[]){
+        int[] nums = {1,2,3};
+
+        System.out.println(Arrays.toString(getConcatenation(nums)));
+
+    }
+
+}
